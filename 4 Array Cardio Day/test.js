86ab78fa5d8f957b69f33740440b853fca5e6d8d
你的名字 <>
2020-03-30 21:37:45 +0800

    数组的map、reduce、filter的练习

---
+++ 4 Array Cardio Day/test.js
@@ -0,0 +1,9 @@
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+var dataS=data.reduce(function(obj,item){
+    if(!obj[item]){
+        obj[item]=0;
+    }
+     obj[item]++;
+    return obj;
+},{})
+console.log(dataS);
